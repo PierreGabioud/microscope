669d9997f8dcef9e5963c20a540469159da014b5
Pierre Gabioud <pierregabioud@gmail.com>
2015-01-27 12:15:41 +0100

    Testing out

--- client/templates/posts/post_item.js
+++ client/templates/posts/post_item.js
@@ -1,7 +1,7 @@
 Template.postItem.helpers({
   domain: function() {
     var a = document.createElement('a');
-    a.href = this.url;
+    a.href = this.url; // Here at each iteration in post_item.html the "this" becomes the object -> here too!
     return a.hostname;
   }
 });
