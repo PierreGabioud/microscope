c0bcaec06172176a0b307e6fb831e44f02fa672e
Pierre Gabioud <pierregabioud@gmail.com>
2015-01-30 11:41:02 +0100

    Released package for errors

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -15,12 +15,12 @@ Template.postSubmit.events({
 
 		Meteor.call('postInsert', post, function(error, result){
 			if(error){
-				return throwError(error.reason);
+				return Errros.throw(error.reason);
 			}
 
 			//Show this result but route anyway because we still have the id of the existing post
 			if(result.postExists){
-				throwError('This link has already been posted');
+				Errors.throw('This link has already been posted');
 			}
 
 			Router.go('postPage', {_id: result._id});
